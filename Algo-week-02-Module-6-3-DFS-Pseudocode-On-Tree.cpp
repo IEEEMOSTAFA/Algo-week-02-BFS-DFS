e87efd28144559ec65c7ab034b4584393dd362dd
IEEEMOSTAFA <121746399+IEEEMOSTAFA@users.noreply.github.com>
2023-07-13 00:56:55 +0600

    Add files via upload

---
+++ Algo-week-02-Module-6-3-DFS-Pseudocode-On-Tree.cpp
@@ -0,0 +1,11 @@
+// void dfs(int node)  //It's for Binary Tree
+// {
+//     cout<<node<<endl;
+//     //base case
+//     if node does not have any child:
+//     return
+//     //general case
+//     dfs(node.left_child);
+//     dfs(node.left_child);
+
+// }
